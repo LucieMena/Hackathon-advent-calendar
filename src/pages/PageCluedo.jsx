cacc31ee184e00abc01c77505dc2c6f4696de828
RUDY MARTINS <martinsosr@gmail.com>
2023-11-23 18:27:25 +0100

    page-cluedo

---
+++ src/pages/PageCluedo.jsx
@@ -0,0 +1,37 @@
+import "./PageCluedo.css";
+
+function PageCluedo() {
+
+    return (
+
+<>
+<h1 className="main-title">Aide le Pere Michel &#x1F50D;</h1>
+<img src="https://www.alunira.com/IMG/arton19.png?1472629116" alt="une-image" className="img"></img>
+<div>
+<figcaption className='explication'>
+  <h2>Accessoires : </h2>
+  <p>Vous n'avez juste qu'à téléchargez le scénario !</p>
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3bDbiE_WCI5nGWNy05rBG5uLUE3N4_paLNg&usqp=CAU" className="word-tof"></img>
+  <h2>Explications : </h2>
+  <p>Aide le Pere Michel qui a perdu son chat &#128570; !</p>
+  <p>Ce matin le Père Michel à ouvert la fenetre et Kiki s'est enfui ! &#128551; </p>
+  <p>Avec l'aide de Papa et Maman (et du scénario !) réalise ta première enquête afin de le retrouver !</p>
+  <p>Tu pourrais commencer par aller voir la Mère Michelle ? &#x1F606;</p>
+</figcaption>
+</div>
+<div className="avis">
+ <h2> Avis :  </h2>  
+ <article className="article1">
+    <h5>C'était super ! &#x1F495;&#x1F495;&#x1F495;&#x1F495;&#x1F495; 5/5</h5>
+    <p>J'ai joué à ce scénario avec mes 15 enfants, ils ont tout simplement adoré !</p>
+    <p>Brian B. - Paris 20</p>
+ </article>
+ <article className="article2">
+    <h5>Principe interessant &#x1F495;&#x1F495;&#x1F495;&#x1F495; 4/5</h5>
+    <p>Un jeu basé sur l'observation et la déduction. Très convivial. Ambiance et bonne humeur garanties !</p>
+    <p>Mickael L. - Paris 20</p>
+ </article>
+</div>
+</>
+)};
+export default PageCluedo;
