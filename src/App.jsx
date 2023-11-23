13dc78b451b72f1c203b07f8152917ab45e666a6
LuluMena <lucie.menage@gmail.com>
2023-11-23 14:53:20 +0100

    carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
 import './App.css'
+import Slider from 'react-slick'
+// import SimpleSlider from './components/Slider';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 1000,
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h2> Adevent Calendar</h2>
+      <Slider {...settings}>
+        <div>
+          <h3>SLIDE 1</h3>
+        </div>
+        <div>
+          <h3>SLIDE 2</h3>
+        </div>
+        <div>
+          <h3>SLIDE 3</h3>
+        </div>
+        <div>
+          <h3>SLIDE 4</h3>
+        </div>
+        <div>
+          <h3>SLIDE 5</h3>
+        </div>
+        <div>
+          <h3>SLIDE 6</h3>
+        </div>
+      </Slider>
+    </div>
+  );
 }
 
 export default App
